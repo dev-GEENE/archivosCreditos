8054582b13ce92359c9c8fe62f073d1796449aae
Barbara Cortez <barbara.cortez@geene.com.py>
2024-10-18 16:03:25 -0300

    cambios de estilos al contenedor y agrego variable al js

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ document.getElementById('fileform').addEventListener('submit', function(event) {
 
     // Crear un objeto FormData con los datos del formulario
     const formData = new FormData(document.getElementById('fileform'));
-    formData.append('leadCode', codigoLead);  // Añadir el leadCode capturado
+    formData.append('leadCode', codigoLead, leadCode);  // Añadir el leadCode capturado
 
     // Realizar la solicitud fetch con la URL y el leadCode dinámico
     fetch(`https://prod-12.brazilsouth.logic.azure.com:443/workflows/3a39a1f99dd94be4b403b0bdcfdce619/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=OWd2VCp0IR7QnXioG8UvWVx-ZekBVJ55uPkI99-IRo4&leadCode=${codigoLead}`, {
